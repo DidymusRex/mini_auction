6f7cc413ee9fbf8845f8fd5aafbf789ac1faeb6b
John Grover <grover.john@gmail.com>
2017-11-18 11:53:14 -0500

    updated model, removing inheritance from Contacts

--- model.py
+++ model.py
@@ -57,10 +57,16 @@ class Items(db.Model):
     is_sold = db.Column(db.Boolean)
 
 
+class Pictures(db.Model):
+    """
+    Images should be in the static directory
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    file_name = db.Column(db.String(255))
+    thumb_name = db.Column(db.String(255))
+
+
 class Contacts(db.Model):
-    """
-    Base contacts class list
-    """
     id = db.Column(db.Integer, primary_key=True)
     last_name = db.Column(db.String(255))
     first_name = db.Column(db.String(255))
@@ -75,30 +81,71 @@ class Contacts(db.Model):
     email_address = db.Column(db.String(255))
 
 
-class Users(UserMixin, Contacts):
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    last_name = db.Column(db.String(255))
+    first_name = db.Column(db.String(255))
+    title = db.Column(db.String(255))
+    address1 = db.Column(db.String(255))
+    address2 = db.Column(db.String(255))
+    city = db.Column(db.String(255))
+    state = db.Column(db.String(255))
+    postal_code = db.Column(db.String(255))
+    telephone = db.Column(db.String(255))
+    telephone_ext = db.Column(db.String(255))
+    email_address = db.Column(db.String(255))
     username = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255), nullable=False)
 
 
-class Donors(Contacts):
+class Donors(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    last_name = db.Column(db.String(255))
+    first_name = db.Column(db.String(255))
+    title = db.Column(db.String(255))
+    address1 = db.Column(db.String(255))
+    address2 = db.Column(db.String(255))
+    city = db.Column(db.String(255))
+    state = db.Column(db.String(255))
+    postal_code = db.Column(db.String(255))
+    telephone = db.Column(db.String(255))
+    telephone_ext = db.Column(db.String(255))
+    email_address = db.Column(db.String(255))
     is_business = db.Column(db.Boolean)
     is_prospect = db.Column(db.Boolean)
     is_donor = db.Column(db.Boolean)
     business_name = db.Column(db.String(255))
 
 
-class Bidders(Contacts):
+class Bidders(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    last_name = db.Column(db.String(255))
+    first_name = db.Column(db.String(255))
+    title = db.Column(db.String(255))
+    address1 = db.Column(db.String(255))
+    address2 = db.Column(db.String(255))
+    city = db.Column(db.String(255))
+    state = db.Column(db.String(255))
+    postal_code = db.Column(db.String(255))
+    telephone = db.Column(db.String(255))
+    telephone_ext = db.Column(db.String(255))
+    email_address = db.Column(db.String(255))
     balance_due = db.Column(db.Float)
 
 
-class Volunteers(Contacts):
+class Volunteers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    last_name = db.Column(db.String(255))
+    first_name = db.Column(db.String(255))
+    title = db.Column(db.String(255))
+    address1 = db.Column(db.String(255))
+    address2 = db.Column(db.String(255))
+    city = db.Column(db.String(255))
+    state = db.Column(db.String(255))
+    postal_code = db.Column(db.String(255))
+    telephone = db.Column(db.String(255))
+    telephone_ext = db.Column(db.String(255))
+    email_address = db.Column(db.String(255))
     event_id = db.Column(db.Integer, db.ForeignKey(Events.id))
 
 
-class Pictures(db.Model):
-    """
-    Images should be in the static directory
-    """
-    id = db.Column(db.Integer, primary_key=True)
-    file_name = db.Column(db.String(255))
-    thumb_name = db.Column(db.String(255))
